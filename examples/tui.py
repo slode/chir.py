7160e6256fd1efa52d33df3208de5f0dee55a393
Stian Lode <stian.lode@gmail.com>
2024-02-15 22:09:13 +0100

    Upgrades the tui

--- examples/tui.py
+++ examples/tui.py
@@ -5,7 +5,7 @@ from textual import work
 from textual.app import App, ComposeResult
 from textual.containers import VerticalScroll
 from textual.widget import Widget
-from textual.widgets import Input, Label, Header, Pretty
+from textual.widgets import Input, Label, Header, Pretty, Tabs, Tab, Markdown
 from textual.logging import TextualHandler
 
 from chirpy import client
@@ -29,7 +29,7 @@ class MessageBox(Widget, can_focus=True):  # type: ignore[call-arg]
 
     def compose(self) -> ComposeResult:
         """Yield message component."""
-        label = Label(self.text, classes=f"message {self.role}")
+        label = Markdown(self.text, classes=f"message {self.role}")
         label.border_title = self.title
         yield label
 
@@ -39,42 +39,68 @@ class ChirpyApp(App):
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self.client = None
+        self.client = client.ChatApiClient(client.url)
+        self.current_session = None
+        self.sessions = {}
+
+    def get_current_session(self):
+        tabs = self.query_one("#session_tabs")
+        return tabs.active[4:] if tabs.active is not None else None
 
     def compose(self) -> ComposeResult:
         yield Header()
+        with Tabs(id="session_tabs"):
+            ...
+
         with VerticalScroll(id="results-container"):
             ...
+
         yield Input(id="message_input", placeholder="Type message")
 
     async def on_mount(self) -> None:
         """Called when app starts."""
         # Give the input focus, so we can start typing straight away
         self.query_one(Input).focus()
+        await self.log_in()
         self.t = asyncio.create_task(self.listen())
 
-    async def listen(self):
-        self.client = client.ChatApiClient(client.url)
+    async def add_session(self, session_id: str):
+        try:
+            self.query_one(f"#tab-{session_id}")
+            return
+        except Exception:
+            ...
+        tabs = self.query_one("#session_tabs").add_tab(Tab(session_id, id=f"tab-{session_id}"))
+        self.refresh()
+
+    async def join_session(self):
+        session = await self.client.chat_session()
+        await self.add_session(session["id"])
+
+    async def log_in(self):
         await self.client.guest_login()
-        user = await self.client.me()
+        self.user = await self.client.me()
+
+    async def listen(self):
         box = self.query_one("#results-container")
         while True:
             try:
-                self.chat = await self.client.chat_session()
-                self.client.dest = self.chat["id"]
                 async for event in self.client.listen():
-                    session = event["session"]
+                    session_id = event["session"]
+                    await self.add_session(session_id)
+
                     source_id = event["origin"]["id"]
                     source = event["origin"]["username"]
                     message = event["message"]
-                    role = "question" if source_id == user["id"] else "answer"
+                    role = "question" if source_id == self.user["id"] else "answer"
                     box.mount(
-                        MessageBox(text=message.strip(), title=f"{source}@{session}", role=role)
+                        MessageBox(text=message.strip(), title=f"{source}@{session_id}", role=role)
                     )
                     box.scroll_end()
                     self.refresh()
             except Exception as e:
                 self.log(e)
+                await asyncio.sleep(3)
 
     async def on_input_submitted(self) -> None:
         """A coroutine to handle a text changed message."""
@@ -95,36 +121,23 @@ class ChirpyApp(App):
             box = self.query_one("#results-container")
             box.mount(Pretty(me, name="/me", classes="message info"))
             box.scroll_end()
-        elif word.startswith("/new "):
-            await self.client.guest_login()
-        elif word.startswith("/logon "):
+        elif word.startswith("/new"):
+            await self.join_session()
+        elif word.startswith("/logon"):
             cmd, user, password = word.split(" ")
             await self.client.login(user, password)
-        elif word.startswith("/invite "):
-            await self.client.invite(self.client.dest, word[7:].strip())
+        elif word.startswith("/invite"):
+            cmd, user = word.split(" ")
+            await self.client.invite(self.get_current_session(), user.strip())
         elif word.startswith("/sessions"):
             sessions = await self.client.sessions()
             box = self.query_one("#results-container")
             box.mount(Pretty(sessions, name="/sessions", classes="message info"))
             box.scroll_end()
-        elif word.startswith("/session "):
-            if word[10:]:
-                self.client.dest = word[9:].strip()
-            else:
-                self.client.dest = self.chat["id"]
-            box = self.query_one("#results-container")
-            box.mount(
-                MessageBox(
-                    text=f"Now writing to session {self.client.dest}",
-                    title="/sessions",
-                    role="info",
-                )
-            )
-            box.scroll_end()
         elif word.startswith("/"):
             return
         else:
-            await self.client.post(self.client.dest, word)
+            await self.client.post(self.get_current_session(), word)
 
         self.query_one(Input).clear()
 
